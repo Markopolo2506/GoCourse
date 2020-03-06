5349a34730d4ea16cbcd171f105905d107d62cac
Alexei Makaryuha <markopolo250688@gmail.com>
2020-03-06 12:42:39 +0300

    Задание 8

---
+++ Lesson8/2/2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	for _, url := range os.Args[1:] {
+		fetch(url)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2fs %7d %s\n", time.Since(start).Seconds(), nbytes, url)
+}
